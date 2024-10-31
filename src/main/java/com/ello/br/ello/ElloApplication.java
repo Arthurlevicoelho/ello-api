4d9d9193fc489562b1208937ed8b9f9d1082c29c
Arthur Coelho <arthurlevicoelho@hotmail.com>
2024-10-31 20:13:38 -0300

    first commit

---
+++ src/main/java/com/ello/br/ello/ElloApplication.java
@@ -0,0 +1,13 @@
+package com.ello.br.ello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ello/br/ello/ElloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ello.br.ello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
